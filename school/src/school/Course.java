e68017189ab1582740db2e1e630ecdd44d70391c
unknown <373000800@mvhsrop901-23.vusdnoc.vusd.k12.ca.us>
2015-09-16 11:13:06 -0700

    Initial Commit

---
+++ school/src/school/Course.java
@@ -0,0 +1,13 @@
+
+package school;
+import java.util.ArrayList;
+
+    public class Course 
+    {
+        private static ArrayList<Person> people = new ArrayList<Person>();
+        
+        enum Class
+        {
+           Math,Science,English,PhysicalEducation,Electives,History,Language
+        }
+    }
